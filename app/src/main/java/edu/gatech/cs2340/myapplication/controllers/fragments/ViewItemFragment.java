51d51461c174d2a1b7c27edb8576648daac69646
Reece Gao <rgao@gatech.edu>
2018-10-18 23:18:54 -0400

    created location card

--- app/src/main/java/edu/gatech/cs2340/myapplication/controllers/fragments/ViewItemFragment.java
+++ app/src/main/java/edu/gatech/cs2340/myapplication/controllers/fragments/ViewItemFragment.java
@@ -1,6 +1,43 @@
 package edu.gatech.cs2340.myapplication.controllers.fragments;
 
-import Fragment;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.gatech.cs2340.myapplication.Callback;
+import edu.gatech.cs2340.myapplication.R;
+import edu.gatech.cs2340.myapplication.controllers.LocationCardRecyclerViewAdapter;
+import edu.gatech.cs2340.myapplication.models.LocationEntry;
+import edu.gatech.cs2340.myapplication.models.TheCloud;
 
 public class ViewItemFragment extends Fragment {
+
+    public ViewItemFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_items, container, false);
+
+        RecyclerView rView = view.findViewById(R.id.recycler_view1);
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(new GridLayoutManager(getContext(), 1,
+                RecyclerView.VERTICAL, false));
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) { }
 }
