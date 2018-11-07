d551e63bdde34fe14afdd8b2d75f9dc3069b13b0
Claudia <claudialchu@gatech.edu>
2018-11-06 23:02:53 -0500

    labeled junit method tests

--- app/src/test/java/edu/gatech/cs2340/myapplication/models/TheCloudTest.java
+++ app/src/test/java/edu/gatech/cs2340/myapplication/models/TheCloudTest.java
@@ -1,27 +1,38 @@
 package edu.gatech.cs2340.myapplication.models;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TheCloudTest {
 
+    @Before
+    public void setUp() {
+
+    }
+
+    // Raj
     @Test
     public void signIn() {
     }
 
+    // Chloe
     @Test
     public void addInventoryEntry() {
     }
 
+    // Collin
     @Test
     public void registerUser() {
     }
 
+    // Claudia
     @Test
     public void getLocations() {
     }
 
+    // Reece?
     @Test
     public void getInventory() {
     }
