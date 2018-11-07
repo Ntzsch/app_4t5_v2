0ae2f98c88b1f61747f395c30592d3f11e785791
Claudia <claudialchu@gatech.edu>
2018-11-06 22:51:26 -0500

    Added single file for all junit testing for TheCloud methods

---
+++ app/src/test/java/edu/gatech/cs2340/myapplication/models/TheCloudTest.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs2340.myapplication.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TheCloudTest {
+
+    @Test
+    public void signIn() {
+    }
+
+    @Test
+    public void addInventoryEntry() {
+    }
+
+    @Test
+    public void registerUser() {
+    }
+
+    @Test
+    public void getLocations() {
+    }
+
+    @Test
+    public void getInventory() {
+    }
+}
